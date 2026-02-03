e3f4487dfbe3325634955fa8d0fcea5e9bf5ac64
Aleksan-dres <galeks382@gmail.com>
2026-02-03 16:24:20 +0300

    Update admin.py
    конечная конфигурация админки

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint, Owner
 
 
-admin.site.register(Flat)
+class OwnerInline(admin.TabularInline): 
+    model = Owner.flats.through 
+    raw_id_fields = ('owner',) 
+    extra = 0 
+
+    
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ["owner","town","address"] 
+    readonly_fields = ["created_at"]  
+    list_display = ['address','price','new_building','construction_year','town', "get_owners"] 
+    inlines = [OwnerInline]
+    def get_owners(self, obj): 
+        return ", ".join([owner.full_name for owner in obj.owners.all()])
+    get_owners.short_description = 'Владельцы'
+    list_editable = ['new_building'] 
+    list_filter = ['new_building','rooms_number','has_balcony'] 
+    raw_id_fields=['likes']
+
+
+class ComplaintAdmin(admin.ModelAdmin): 
+    raw_id_fields = ['flat'] 
+    list_display = ['user', 'flat', 'text']  
+
+
+class OwnerAdmin(admin.ModelAdmin): 
+    raw_id_fields = ["flats"]
+    list_display = ["full_name", "phonenumber", "pure_phone"] 
+
+
+admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Owner, OwnerAdmin) 
